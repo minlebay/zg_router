59ad033394430fe8929648440cc8148c9d662c64
Ilshat Minnibaev <ilshatminnibaev@gmail.com>
2024-08-03 22:24:17 +0200

    redundant funcs deleted

--- internal/app/config.go
+++ internal/app/config.go
@@ -1,11 +1,9 @@
 package app
 
 import (
-	"fmt"
 	"go.uber.org/config"
 	"go.uber.org/fx"
 	"os"
-	"strings"
 )
 
 type Config struct {
@@ -44,50 +42,4 @@ func NewConfig() (ResultConfig, error) {
 		Provider: yamlProvider,
 		Config:   config,
 	}, nil
-}
-
-func convertMapKeysToStrings(m map[interface{}]interface{}) map[string]interface{} {
-	newMap := make(map[string]interface{})
-	for k, v := range m {
-		strKey := fmt.Sprintf("%v", k)
-		switch val := v.(type) {
-		case map[interface{}]interface{}:
-			newMap[strKey] = convertMapKeysToStrings(val)
-		default:
-			newMap[strKey] = val
-		}
-	}
-	return newMap
-}
-
-func replaceEnvVariables(config map[string]interface{}) {
-	for key, value := range config {
-		switch v := value.(type) {
-		case map[string]interface{}:
-			replaceEnvVariables(v)
-		case []interface{}:
-			replaceEnvVariablesInSlice(v)
-		case string:
-			if strings.HasPrefix(v, "${") && strings.HasSuffix(v, "}") {
-				envVar := v[2 : len(v)-1]
-				config[key] = os.Getenv(envVar)
-			}
-		}
-	}
-}
-
-func replaceEnvVariablesInSlice(s []interface{}) {
-	for i, value := range s {
-		switch v := value.(type) {
-		case map[string]interface{}:
-			replaceEnvVariables(v)
-		case []interface{}:
-			replaceEnvVariablesInSlice(v)
-		case string:
-			if strings.HasPrefix(v, "${") && strings.HasSuffix(v, "}") {
-				envVar := v[2 : len(v)-1]
-				s[i] = os.Getenv(envVar)
-			}
-		}
-	}
 }
